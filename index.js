e0b401ee822bbed776c46ebed5101893989b6b94
DeveloFer77 <fernando.proff@hotmail.com>
2021-06-25 19:36:57 -0300

    update:menu=>inquirer.pausa

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const colors = require('colors')
-const { inquirerMenu } = require('./helpers/inquirer')
+const { inquirerMenu, pausa } = require('./helpers/inquirer')
 
 console.clear()
 
@@ -12,9 +12,8 @@ const main = async () => {
     do {
         opt = await inquirerMenu();
         console.log({ opt })
-        // if( opt !== '0'){
-        //     await pausa()
-        // }
+
+        await pausa(); 
         
     } while (opt !== '0');
 
