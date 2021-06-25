90ff324129d6415fedc2897c94dc157e9a07a9c2
DeveloFer77 <fernando.proff@hotmail.com>
2021-06-25 19:09:17 -0300

    iniciamos la incorporacion de inquirer

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const colors = require('colors')
-const { mostrarMenu, pausa } = require('./helpers/mensajes')
+const { inquirerMenu } = require('./helpers/inquirer')
+
 console.clear()
 
 const main = async () => {
@@ -9,11 +10,11 @@ const main = async () => {
     let opt = ''
 
     do {
-        opt = await mostrarMenu();
+        opt = await inquirerMenu();
         console.log({ opt })
-        if( opt !== '0'){
-            await pausa()
-        }
+        // if( opt !== '0'){
+        //     await pausa()
+        // }
         
     } while (opt !== '0');
 
